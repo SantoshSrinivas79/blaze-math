096bc5547e0b30a1304adfbb31c20bcd248f06ae
Manuel <mc.ro18@gmail.com>
2017-04-11 17:24:42 -0600

    Add support for Math native methods

--- math.js
+++ math.js
@@ -6,6 +6,7 @@ function math () {
     div ( ...terms ) { return result( terms, ( a, b ) => b ? a / b : 0 ); },
     sum ( ...terms ) { return result( terms, ( a, b ) => a + b ); },
     sub ( ...terms ) { return result( terms, ( a, b ) => a - b ); },
+    method ( name, number, options ) { return Math[ name ]( number, options ); },
   }
 }
 
